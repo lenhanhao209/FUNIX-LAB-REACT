889d42a08245ebaf17141fb60702638e570eba10
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-05-17 17:19:24 +0700

    Lab11-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from "react";
+import React from "react";
 import DishDetail from "./DishDetailComponent";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
@@ -7,26 +7,21 @@ import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { propTypes } from "redux-form";
 
 const Main = () => {
   const dishes = useSelector((state) => state.dishes);
   const comments = useSelector((state) => state.comments);
   const leaders = useSelector((state) => state.leaders);
-
+  const location = useLocation();
   return (
     <div>
       <Header />
       <TransitionGroup>
-        <CSSTransition
-          // key={this.props.locations.key}
-          classNames="page"
-          timeout={300}
-        >
+        <CSSTransition key={location.key} classNames="page" timeout={300}>
           <Routes>
             <Route path="/home" element={<Home />} />
             <Route exact path="/menu" element={<Menu />} />
